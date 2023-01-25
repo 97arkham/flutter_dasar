60c42e8a06dff7d986eaeaee5552b2944d1c3b64
97arkham <khairunarkham97@gmail.com>
2023-01-25 10:38:01 +0700

    project belajar flutter ketiga

---
+++ flutter_application_hari_3/test/widget_test.dart
@@ -0,0 +1,15 @@
+void main() {
+  int hasilPenjumlahan = penjumlahan(1, 7, 8);
+  print(hasilPenjumlahan);
+
+  pengurangan(1, 7);
+}
+
+int penjumlahan(int angka1, int angka2, int angka3) {
+  int hasil = angka1 + angka2 + angka3;
+  return hasil;
+}
+
+void pengurangan(int angka1, angka2) {
+  print(angka1 - angka2);
+}
